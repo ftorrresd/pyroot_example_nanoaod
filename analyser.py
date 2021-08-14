524210792b16909faacbd06eb723720487ed225e
Felipe Silva <felipe.silva@cern.ch>
2021-08-14 15:28:44 +0200

    Add unteste TChain

--- analyser.py
+++ analyser.py
@@ -1,6 +1,6 @@
 import tqdm 
 import ROOT
-
+#.....
 # setup some ROOT stuff
 ROOT.PyConfig.DisableRootLogon = True
 ROOT.PyConfig.IgnoreCommandLineOptions = False
@@ -9,53 +9,222 @@ ROOT.gROOT.SetBatch(True)
 # Config
 max_events = 25000 # max number of events to be processed
 
+
+#calculating weight of MC data
+crosssection = 20508.9
+luminosity = 1
+W_mc = (crosssection*luminosity)/max_events
+W_mc = 0.82
+
+#weight for CMS data
+W=1
+
 # Declare histograms
 # muons p_T
-h_muon_pt = ROOT.TH1F("h_muon_pt", "; Muon pT ; Muons", 50, 0, 200)
+h_muon_pt_1 = ROOT.TH1F("h_muon_pt_1", "; Muon pT ; Muons", 50, 0, 200)
 # force the storage and computation of the sum of the square of weights per bin
 # REF: https://root.cern.ch/doc/master/classTH1.html#associated-errors
-h_muon_pt.Sumw2() 
+h_muon_pt_1.Sumw2()
+
+h_muon_pt_2 = ROOT.TH1F("h_muon_pt_2", "; Muon pT ; Muons", 50, 0, 200)
+# force the storage and computation of the sum of the square of weights per bin
+# REF: https://root.cern.ch/doc/master/classTH1.html#associated-errors
+h_muon_pt_2.Sumw2()
+
+# muons phi
+h_muon_phi_1 = ROOT.TH1F("h_muon_phi_1", "; Muon phi ; Muons", 50, -4, 4)
+h_muon_phi_1.Sumw2()
+
+h_muon_phi_2 = ROOT.TH1F("h_muon_phi_2", "; Muon phi ; Muons", 50, -4, 4)
+h_muon_phi_2.Sumw2()
+
+# MET Et
+h_MET_phi_1 = ROOT.TH1F("h_MET_phi_1", "; MET phi ; Muons", 50, -5, 5)
+h_MET_phi_1.Sumw2()
+
+h_MET_phi_2 = ROOT.TH1F("h_MET_phi_2", "; MET phi ; Muons", 50, -5, 5)
+h_MET_phi_2.Sumw2()
+
+#MET pt
+h_MET_pt_1 = ROOT.TH1F("h_MET_pt_1", "; MET pt ; Muons", 50, 0, 200)
+h_MET_pt_1.Sumw2()
+
+h_MET_pt_2 = ROOT.TH1F("h_MET_pt_2", "; MET pt ; Muons", 50, 0, 200)
+h_MET_pt_2.Sumw2()
 
 # number of muons per event
-h_nmuon = ROOT.TH1F("h_nmuon", "; Number of muons ; Events", 5, 0, 5)
-h_nmuon.Sumw2() 
+h_nmuon_1 = ROOT.TH1F("h_nmuon_1", "; Number of muons ; Events", 5, 0, 5)
+h_nmuon_1.Sumw2() 
 
-# Load ROOT file 
-data_file = ROOT.TFile.Open("root://cms-xrd-global.cern.ch///store/data/Run2018D/SingleMuon/NANOAOD/UL2018_MiniAODv1_NanoAODv2-v1/280000/0BF2854F-C799-2F41-A0F4-0274DC311372.root")
-# data_file = ROOT.TFile.Open("/eos/cms/store/data/Run2018D/SingleMuon/NANOAOD/UL2018_MiniAODv1_NanoAODv2-v1/280000/0BF2854F-C799-2F41-A0F4-0274DC311372.root")
+h_nmuon_2 = ROOT.TH1F("h_nmuon_2", "; Number of muons ; Events", 5, 0, 5)
+h_nmuon_2.Sumw2() 
+
+#invariant mass of muon and MET (CMS DATA)
+h_MmM1 = ROOT.TH1F("h_MmM1","Transverse mass histogram (CMS DATA)", 50, 60, 200)
+h_MmM1.Sumw2()
+
+#invariant mass of muon and MET (Simulated DATA)
+h_MmM2 = ROOT.TH1F("h_MmM2","Transverse mass histogram (MC DATA)", 50, 0, 200)
+h_MmM2.Sumw2()
+
+# invariant mass of two muons
+h_Mmm = ROOT.TH1F("h_Mmm","Invariant mass of two muons", 50, 70, 100)
+h_Mmm.Sumw2()
+
+# Create a Chain of filles to analyze
+data_chain = ROOT.TChain("Events")
+data_chain.Add("/eos/cms/store/data/Run2018D/SingleMuon/NANOAOD/UL2018_MiniAODv1_NanoAODv2-v1/280000/0BF2854F-C799-2F41-A0F4-0274DC311372.root")
+
+mc_chain = ROOT.TChain("Events")
+mc_chain.Add("root://cms-xrd-global.cern.ch///store/mc/RunIISummer20UL18NanoAODv2/WJetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8/NANOAODSIM/106X_upgrade2018_realistic_v15_L1v1-v1/00000/B81103CC-8168-3F46-AEC4-6CEF3012FC8A.root")
+
+# # Load ROOT file g
+# #data_file = ROOT.TFile.Open("root://cms-xrd-global.cern.ch///store/data/Run2018D/SingleMuon/NANOAOD/UL2018_MiniAODv1_NanoAODv2-v1/280000/0BF2854F-C799-2F41-A0F4-0274DC311372.root")
+# data_file = ROOT.TFile.Open("/eos/cms/store/data/Run2018D/SingleMuon/NANOAOD/UL2018_MiniAODv1_NanoAODv2-v1/280000/0BF2854F-C799-2F41-A0F4-0274DC311372.root") #(REAL CMS DATA)
+
+# #MC data file
+# mc_file = ROOT.TFile.Open("root://cms-xrd-global.cern.ch///store/mc/RunIISummer20UL18NanoAODv2/WJetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8/NANOAODSIM/106X_upgrade2018_realistic_v15_L1v1-v1/00000/B81103CC-8168-3F46-AEC4-6CEF3012FC8A.root")
 
 # total number of events in the file
-n_events = data_file.Events.GetEntries() 
+n_events1 = data_chain.GetEntries()  #(REAL CMS DATA)
+
+n_events2 = mc_chain.GetEntries()
 
 # Set the number of events for percentage calc
-n_events_for_tqdm = n_events
+n_events1_for_tqdm = n_events1
 if max_events >= 0:
-    n_events_for_tqdm = max_events
+    n_events1_for_tqdm = max_events
+    
+n_events2_for_tqdm = n_events2
+if max_events >= 0:
+    n_events2_for_tqdm = max_events 
 
 # loop over events and fill histograms
-for ievt, evt in tqdm.tqdm(enumerate(data_file.Events), desc ="Events", total= n_events_for_tqdm):
+counterA = 0
+counterB = 0
+counterC = 0
+counterD = 0
+counter1 = 0
+count1 = 0
+
+print "Processing CMS Data events..."
+for ievt, evt in tqdm.tqdm(enumerate(data_chain), desc ="Events", total= n_events1_for_tqdm):     #(REAL CMS DATA)
+    if ievt+1 > max_events and max_events > 0:
+        break
+    # loop over muons
+    if evt.nMuon>=1:
+      if evt.HLT_IsoMu24==1:
+        for i_muon in range(evt.nMuon):
+          if evt.Muon_pt > 26:
+            if evt.MET_pt > 40: 
+              if evt.Muon_pfRelIso03_all[i_muon] < 0.35:
+                if abs(evt.Muon_eta[i_muon]) < 2.5:
+                  muon1 = ROOT.Math.PtEtaPhiMVector(evt.Muon_pt[0], evt.Muon_eta[0], evt.Muon_phi[0], evt.Muon_mass[0])
+                  MET1 = ROOT.Math.PtEtaPhiMVector(evt.MET_pt, 0, evt.MET_phi, 0)
+                  TM_mM1 = muon1 + MET1
+                  m_mM1 = TM_mM1.M()
+                  if evt.Muon_tightId[i_muon]==1:
+                    if (m_mM1 > 60 and m_mM1 < 200):
+                      h_muon_pt_1.Fill(evt.Muon_pt[i_muon],W)
+                      h_muon_phi_1.Fill(evt.Muon_phi[i_muon],W) 
+                      h_MmM1.Fill(m_mM1,W)
+                      h_MET_phi_1.Fill(evt.MET_phi,W)
+                      h_MET_pt_1.Fill(evt.MET_pt,W)
+                      h_nmuon_1.Fill(evt.nMuon,W)   # per event info
+                      counterA+=1
+                    else:
+                      counterD+=1
+                  else:
+                    if (m_mM1 > 60 and m_mM1 < 200):
+                      counterB+=1
+                    else:
+                      counterC+=1
+                    counter1+=1  
+                  break
+    
+    count1+=1          
+    
+print(counterA)  
+print(counterB)
+print(counterC) 
+print(counterD)
+print(counter1)
+print(count1) 
+
+
+countera = 0
+counterb = 0
+counterc = 0
+counterd = 0 
+counter2 = 0
+count2 = 0
+     
+print "Processing CMS MC events..."
+for ievt, evt in tqdm.tqdm(enumerate(mc_chain), desc ="Events", total= n_events2_for_tqdm):
     # condition to stop event processing, when max_events is reached
     if ievt+1 > max_events and max_events > 0:
         break
+    # loop over muons 
+    if evt.nMuon>=1:
+      if evt.HLT_IsoMu24==1:         
+        for i_muon in range(evt.nMuon):
+          if evt.Muon_pt > 26:
+            if evt.MET_pt > 40:
+              if evt.Muon_pfRelIso03_all[i_muon] < 0.35:
+                if abs(evt.Muon_eta[i_muon]) < 2.5:     
+                  muon2 = ROOT.Math.PtEtaPhiMVector(evt.Muon_pt[0], evt.Muon_eta[0], evt.Muon_phi[0], evt.Muon_mass[0])
+                  MET2 = ROOT.Math.PtEtaPhiMVector(evt.MET_pt, 0, evt.MET_phi, 0)
+                  TM_mM2 = muon2 + MET2
+                  m_mM2 = TM_mM2.M()
+                  if evt.Muon_tightId[i_muon]==1:
+                    if (m_mM2 > 60 and m_mM2 < 200):
+                      h_muon_pt_2.Fill(evt.Muon_pt[i_muon],W_mc)
+                      h_muon_phi_2.Fill(evt.Muon_phi[i_muon],W_mc) 
+                      h_MmM2.Fill(m_mM2,W_mc)
+                      h_MET_phi_2.Fill(evt.MET_phi,W_mc)
+                      h_MET_pt_2.Fill(evt.MET_pt,W_mc)
+                      h_nmuon_2.Fill(evt.nMuon,W_mc)
+                      countera+=1
+                    else:
+                      counterd+=1
+                  else:
+                    if (m_mM2 > 60 and m_mM2 < 200):
+                      counterb+=1
+                    else:
+                      counterc+=1
+                    counter2+=1  
+                  break
+                 
+    count2+=1                    
 
-    # per event info
-    h_nmuon.Fill(evt.nMuon)
-
-    # loop over muons
-    for i_muon in range(evt.nMuon):
-        h_muon_pt.Fill(evt.Muon_pt[i_muon])
-
-
+print(countera)  
+print(counterb)
+print(counterc)
+print(counterd)
+print(counter2)
+print(count2)     
+    
+          
 # Save histogram to a file
 # Open a ROOT file and save the formula, function and histogram
 output_file = ROOT.TFile( 'output_file.root', 'RECREATE')
 
 # Write histograms to file
-h_muon_pt.Write()
-h_nmuon.Write()
+h_muon_pt_1.Write()
+h_muon_phi_1.Write()
+h_MET_phi_1.Write()
+h_MET_pt_1.Write()
+h_nmuon_1.Write()
+h_muon_pt_2.Write()
+h_muon_phi_2.Write()
+h_MET_phi_2.Write()
+h_MET_pt_2.Write()
+h_nmuon_2.Write()
+h_MmM1.Write()
+h_MmM2.Write()
+
 
 # Close file
 output_file.Close()
 
 print "\nOutput histograms have been saved output_file.root"
-    
---
+++ stac.py
@@ -0,0 +1,149 @@
+import ROOT
+ROOT.gROOT.SetBatch(True) 
+# setup some ROOT stuff
+ROOT.PyConfig.DisableRootLogon = True
+ROOT.PyConfig.IgnoreCommandLineOptions = False
+ROOT.gROOT.SetBatch(True)
+
+# Open a ROOT file 
+output_file = ROOT.TFile( 'output_file.root')
+
+# save the histogram
+h_muon_pt_1 = output_file.Get("h_muon_pt_1")
+h_muon_phi_1 = output_file.Get("h_muon_phi_1")
+h_MET_phi_1 = output_file.Get("h_MET_phi_1")
+h_MET_pt_1 = output_file.Get("h_MET_pt_1")
+h_nmuon_1 = output_file.Get("h_nmuon_1")
+h_muon_pt_2 = output_file.Get("h_muon_pt_2")
+h_muon_phi_2 = output_file.Get("h_muon_phi_2")
+h_MET_phi_2 = output_file.Get("h_MET_phi_2")
+h_MET_pt_2 = output_file.Get("h_MET_pt_2")
+h_nmuon_2 = output_file.Get("h_nmuon_2")
+h_MmM1 = output_file.Get("h_MmM1")
+h_MmM2 = output_file.Get("h_MmM2")
+
+# setting the title
+h_muon_pt_1.SetTitle("Data")
+h_muon_phi_1.SetTitle("Data")
+h_MET_phi_1.SetTitle("Data")
+h_MET_pt_1.SetTitle("Data")
+h_nmuon_1.SetTitle("Data")
+h_muon_pt_2.SetTitle("MC")
+h_muon_phi_2.SetTitle("MC")
+h_MET_phi_2.SetTitle("MC")
+h_MET_pt_2.SetTitle("MC")
+h_nmuon_2.SetTitle("MC")
+h_MmM1.SetTitle("Data")
+h_MmM2.SetTitle("MC")
+
+
+h_muon_pt_1.SetMarkerStyle(ROOT.kFullCircle)
+h_muon_pt_2.SetLineWidth(2)
+h_muon_pt_2.SetFillColorAlpha(ROOT.kBlue, 0.35)
+h_muon_phi_1.SetMarkerStyle(ROOT.kFullCircle)
+h_muon_phi_2.SetLineWidth(2)
+h_muon_phi_2.SetFillColorAlpha(ROOT.kBlue, 0.35)
+h_MmM1.SetMarkerStyle(ROOT.kFullCircle)
+h_MmM2.SetLineWidth(2)
+h_MmM2.SetFillColorAlpha(ROOT.kBlue, 0.75)
+h_MET_pt_1.SetMarkerStyle(ROOT.kFullCircle)
+h_MET_pt_2.SetLineWidth(2)
+h_MET_pt_2.SetFillColorAlpha(ROOT.kBlue, 0.35)
+h_MET_phi_1.SetMarkerStyle(ROOT.kFullCircle)
+h_MET_phi_2.SetLineWidth(2)
+h_MET_phi_2.SetFillColorAlpha(ROOT.kBlue, 0.35)
+h_nmuon_1.SetMarkerStyle(ROOT.kFullCircle)
+h_nmuon_2.SetLineWidth(2)
+h_nmuon_2.SetFillColorAlpha(ROOT.kBlue, 0.35)
+
+
+c1 = ROOT.TCanvas("c1","c1",600,400)
+
+
+h_hs_M = ROOT.THStack("h_hs_M","MC and CMS data for transverse mass")
+h_hs_muon_pt = ROOT.THStack("h_hs_muon_pt","MC and CMS data for muon_pt")
+h_hs_muon_phi = ROOT.THStack("h_hs_muon_phi","MC and CMS data for muon_phi")
+h_hs_MET_pt = ROOT.THStack("h_hs_MET_pt","MC and CMS data for MET_pt")
+h_hs_MET_phi = ROOT.THStack("h_hs_MET_phi","MC and CMS data for MET_phi")
+h_hs_nmuon = ROOT.THStack("h_hs_nmuon","MC and CMS data for nmuon")
+
+h_hs_M.Add(h_MmM1)
+h_hs_M.Add(h_MmM2)
+h_hs_muon_pt.Add(h_muon_pt_1)
+h_hs_muon_pt.Add(h_muon_pt_2)
+h_hs_muon_phi.Add(h_muon_phi_1)
+h_hs_muon_phi.Add(h_muon_phi_2)
+h_hs_MET_pt.Add(h_MET_pt_1)
+h_hs_MET_pt.Add(h_MET_pt_2)
+h_hs_MET_phi.Add(h_MET_phi_1)
+h_hs_MET_phi.Add(h_MET_phi_2)
+h_hs_nmuon.Add(h_nmuon_1)
+h_hs_nmuon.Add(h_nmuon_2)
+
+
+#Drawing Histograms
+h_hs_M.Draw("nostack")
+c1.Modified()
+ROOT.gPad.BuildLegend(0.75,0.75,0.95,0.95,"")
+c1.SaveAs("h_hs_M.png")
+c1.SaveAs("h_hs_M.pdf")
+
+h_hs_muon_pt.Draw("nostack")
+c1.Modified()
+ROOT.gPad.BuildLegend(0.75,0.75,0.95,0.95,"")
+c1.SaveAs("h_hs_muon_pt.png")
+c1.SaveAs("h_hs_muon_pt.pdf")
+
+h_hs_muon_phi.Draw("nostack")
+c1.Modified()
+ROOT.gPad.BuildLegend(0.75,0.75,0.95,0.95,"")
+c1.SaveAs("h_hs_muon_phi.png")
+c1.SaveAs("h_hs_muon_phi.pdf")
+
+h_hs_MET_pt.Draw("nostack")
+c1.Modified()
+ROOT.gPad.BuildLegend(0.75,0.75,0.95,0.95,"")
+c1.SaveAs("h_hs_MET_pt.png")
+c1.SaveAs("h_hs_MET_pt.pdf")
+
+h_hs_MET_phi.Draw("nostack")
+c1.Modified()
+ROOT.gPad.BuildLegend(0.75,0.75,0.95,0.95,"")
+c1.SaveAs("h_hs_MET_phi.png")
+c1.SaveAs("h_hs_MET_phi.pdf")
+
+h_hs_nmuon.Draw("nostack")
+c1.Modified()
+ROOT.gPad.BuildLegend(0.75,0.75,0.95,0.95,"")
+c1.SaveAs("h_hs_nmuon.png")
+c1.SaveAs("h_hs_nmuon.pdf")
+
+'''c1.SaveAs("h_hs_M.png")
+c1.SaveAs("h_hs_M.pdf")
+c1.SaveAs("h_hs_muon_pt.png")
+c1.SaveAs("h_hs_muon_pt.pdf")
+c1.SaveAs("h_hs_muon_phi.png")
+c1.SaveAs("h_hs_muon_phi.pdf")
+c1.SaveAs("h_hs_MET_pt.png")
+c1.SaveAs("h_hs_MET_pt.pdf")
+c1.SaveAs("h_hs_MET_phi.png")
+c1.SaveAs("h_hs_MET_phi.pdf")
+c1.SaveAs("h_hs_nmuon.png")
+c1.SaveAs("h_hs_nmuon.pdf")'''
+
+
+output_file.Close()
+
+'''# open a file that contains stacked histogras
+stacked_file = ROOT.TFile('stacked_file.root', 'RECREATE')
+
+h_hs_M.Write()
+h_hs_muon_pt.Write()
+h_hs_muon_phi.Write()
+h_hs_MET_pt.Write()
+h_hs_MET_phi.Write()
+h_hs_nmuon.Write()
+
+#close file...
+stacked_file.Close()'''
+
